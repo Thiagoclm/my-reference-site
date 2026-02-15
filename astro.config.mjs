580675875092ba4c14241405e8aa31b6848f6ef5
Thiago Cesar Lousada Marsola <thiagoclm@gmail.com>
2026-02-15 19:04:08 -0300

    Update for new repo base path and remove hardcoded /website/

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from '@astrojs/mdx';
 import pagefind from 'astro-pagefind';
 
 export default defineConfig({
-    site: 'https://thiagoclm.github.io/website',
-    base: '/website/',
+    site: 'https://thiagoclm.github.io/my-reference-site',
+    base: '/my-reference-site/',
     integrations: [mdx(), pagefind()],
 });
