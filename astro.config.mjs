7153a4e39532edba231f26945f93f94bc7d98a46
Thiago Cesar Lousada Marsola <thiagoclm@gmail.com>
2026-02-15 18:40:47 -0300

    Fix base path in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import pagefind from 'astro-pagefind';
 
 export default defineConfig({
     site: 'https://thiagoclm.github.io/myreferencesite',
+    base: '/myreferencesite/',
     integrations: [mdx(), pagefind()],
 });
