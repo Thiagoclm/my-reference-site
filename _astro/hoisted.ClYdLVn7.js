59bd05bfe0520ed05b113869d6096156bac495ad
Thiagoclm <Thiagoclm@users.noreply.github.com>
2026-03-01 21:57:44 +0000

    deploy: 3aa1790ea26cc966c7e59afde42a77f6eab0adcb

--- _astro/hoisted.ClYdLVn7.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.Dqd9ecxJ.js";const e=document.getElementById("data-hoje");e&&(e.textContent=new Date().toLocaleDateString("pt-BR",{weekday:"long",year:"numeric",month:"long",day:"numeric"}));
